026718a66f34862db4b0f4d11a7e2560476054c7
Arnaud De Couvreur student <arnaud.de.couvreur@student.howest.be>
2022-10-07 15:46:48 +0200

    dark theme

--- script/app.js
+++ script/app.js
@@ -1,13 +1,18 @@
-function handleFloatingLabel() {
-   
-}
+const passwordToggle = document.querySelector(".c-toggle-password__label");
+const checkbox = document.querySelector(".c-toggle-password__checkbox");
+
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  const passwordOptions = ["password", "text"];
+
+  passwordToggle.addEventListener("change", function () {
+    passwordInput.type = passwordOptions[+this.checked];
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Script loaded!");
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
